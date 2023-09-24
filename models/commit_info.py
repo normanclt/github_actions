81bab53f4061b71b4912a36a4053e1128665e715
normanclt <norman.tecson@gmail.com>
2023-09-25 05:00:16 +0800

    Added dataclass: Commit_Info

--- models/commit_info.py
+++ models/commit_info.py
@@ -1,7 +1,17 @@
 from dataclasses import dataclass
 import typing
+from github.GitCommit import GitCommit
+from github.Repository import Repository
+from github.Branch import Branch
 
 
 @dataclass
-class Commit_info:
+class Commit_Info:
     sha: str
+    committer: str
+    last_modified: str
+
+    def __init__(self, branch: Branch):
+        self.sha = branch.commit.commit.sha
+        self.committer = branch.commit.committer
+        self.last_modified = branch.commit.commit.last_modified

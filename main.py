4cc6136e8d316cb9a72140ecd03cd43a9923221e
normanclt <norman.tecson@gmail.com>
2023-09-25 11:35:08 +0800

    Updated main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from github import Github, Auth
 from icecream import ic
 import os
+from models.commit_info import Commit_Info
 # REFERENCE GITHUB ACTIONS ENVIRONMENT VALUES as constants
 PAT_SUPER_ALL_REPOS = os.environ['PAT_SUPER_ALL_REPOS']
 AWS1_IP = os.environ["AWS1_IP"]
@@ -15,13 +16,16 @@ destination_folders = []
 
 # remote repo
 base_url = "https://github.com/normanclt/github_actions.git"
-remote_repo = Github(base_url=base_url)
 
-repos = remote_repo.get_repo()
 
 # remote repo we will be observing
 # auth
 auth = Auth.Token(PAT_SUPER_ALL_REPOS)
-github01 = Github(auth=auth, base_url=base_url)
+github = Github(auth=auth, base_url=base_url)
 
-print(type(github01))
+staging_repo = github.get_repo("normanclt/staging_repo")
+main_branch = staging_repo.get_branch('main')
+
+main_branch_current_commit = Commit_Info(main_branch)
+
+print(main_branch_current_commit.sha)

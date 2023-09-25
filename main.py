2b766262866f3b68d2aa737f9c3e0635ec5e8763
normanclt <norman.tecson@gmail.com>
2023-09-25 11:05:19 +0800

    -Renamed checkout.yml to file_sync_service.yml -Added actions to file_sync_service.yml -Update main.py to reference environment variables/secrets

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from pathlib import Path
-from git.repo import Repo
-from git.objects.commit import Commit
 from github import Github, Auth
 from icecream import ic
+import os
+# REFERENCE GITHUB ACTIONS ENVIRONMENT VALUES as constants
+PAT_SUPER_ALL_REPOS = os.environ['PAT_SUPER_ALL_REPOS']
+AWS1_IP = os.environ["AWS1_IP"]
+AWS1_SSH_KEY = os.environ["AWS1_SSH_KEY"]
+AWS1_USER = os.environ["AWS1_USER"]
 
 
-local_repo_path = Path(r"D:\DEV\github_actions")
-local_repo = Repo(local_repo_path)
-commits = local_repo.iter_commits()
-remotes = local_repo.remotes
-branches = local_repo.branches
+# SOURCE FOLDERS:
+source_folders = []
+destination_folders = []
 
 # remote repo
 base_url = "https://github.com/normanclt/github_actions.git"
 remote_repo = Github(base_url=base_url)
 
 repos = remote_repo.get_repo()
+
+# remote repo we will be observing
+# auth
+auth = Auth.Token(PAT_SUPER_ALL_REPOS)
+github01 = Github(auth=auth, base_url=base_url)
+
+print(type(github01))

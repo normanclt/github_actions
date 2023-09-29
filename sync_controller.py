73e13e5aa0071897d216ab5ad1b1e111f7812088
Norman Caesar L. Tecson <normancaesar.tecson@hcl.com>
2023-09-29 10:57:30 +0800

    Changed file-like object to bytesIO of ssh_key

--- sync_controller.py
+++ sync_controller.py
@@ -2,7 +2,7 @@ from services.push_providers import *
 from services.config_manager import Config_Manager
 from services.activity_logger import Activity_Logger
 from services.resource_connectors import Github_Connector, SSHFS_Connector
-from io import StringIO
+from io import StringIO, BytesIO
 
 # ENV variables from Github Actions Secret
 PAT_SUPER_ALL_REPOS = os.environ['PAT_SUPER_ALL_REPOS']
@@ -29,7 +29,7 @@ cm.branch_name = 'publish'
 cm.organization_name = 'bigfix'
 cm.ssh_user = 'ec2-user'
 cm.remote_ip = '52.74.243.47'
-cm.ssh_private_key = StringIO(AWS1_SSH_KEY)
+cm.ssh_private_key = BytesIO(AWS1_SSH_KEY)
 # Local
 # cm.ssh_private_key = Path(
 #     r"D:\Projects\keys\aws_instance_1\aws1\aws1_id_ed25519").as_posix()
